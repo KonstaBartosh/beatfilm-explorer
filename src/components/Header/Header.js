0862c92ac54254cf0cb82b3aa9c12cd92c06da5b
Konstantin Bartosh <rustigillo@gmail.com>
2023-08-12 15:44:56 +0700

    Refactor: header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,34 +1,29 @@
 import React from "react";
-
 import Navigation from "../Navigation/Navigation";
 import './Header.css';
 import logo from '../../images/logo.svg'
 
 function Header({ loggedIn }) {
+  const profileMarkup = loggedIn ? (
+    <nav className="header__profile-account">
+      <button className="header__button header__button_account">Аккаунт</button>
+    </nav>
+  ) : (
+    <nav className="header__profile-nav">
+      <button className="header__button header__button_register">Регистрация</button>
+      <button className="header__button header__button_logout">Войти</button>
+    </nav>
+  );
 
-	const profileNavMarkup = (
-		<nav className="header__profile-nav">
-		<button className="header__button header__button_register">Регистрация</button>
-		<button className="header__button header__button_logout">Войти</button>
-	</nav>
-	);
-
-	const myProfileAccMarkup =(
-		<nav className="header__profile-account">
-		<button className="header__button header__button_account">Аккаунт</button>
-	</nav>
-	);
-
-	return(
-		
-		<div className="header">
-			<div className="header__container">
-				<img src={logo} alt="логотип" className="header__logo" />
-				<Navigation/>
-				{loggedIn? myProfileAccMarkup : profileNavMarkup}
-			</div>
-		</div>
-	);
+  return (
+    <div className="header">
+      <div className="header__container">
+        <img src={logo} alt="логотип" className="header__logo" />
+        {loggedIn ? <Navigation /> : null}
+        {profileMarkup}
+      </div>
+    </div>
+  );
 }
 
 export default Header;
