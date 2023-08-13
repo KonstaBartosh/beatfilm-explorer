a95c5883012691c19f1712c5968230cfd07594a1
Konstantin Bartosh <rustigillo@gmail.com>
2023-08-13 22:35:54 +0700

    Feat: add AboutProject

---
+++ src/components/Main/AboutProject/AboutProject.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+import "./AboutProject.css";
+
+export default function AboutProject() {
+  return (
+    <section className="about">
+      <h2 className="about__title about__title_line">О проекте</h2>
+      <div className="about__text-container">
+        <div className="block">
+          <h2 className="block__title">
+						Дипломный проект включал 5 этапов
+					</h2>
+          <p className="block__text">
+            Составление плана, работу над бэкендом, вёрстку, добавление
+            функциональности и финальные доработки.
+          </p>
+        </div>
+        <div className="block">
+          <h2 className="block__title">
+						На выполнение диплома ушло 5 недель
+					</h2>
+          <p className="block__text">
+            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
+            соблюдать, чтобы успешно защититься.
+          </p>
+        </div>
+      </div>
+      <div className="about__timeframe">
+        <div className="about__timeframe-block about__timeframe-block_first-week">
+          <p className="about__timeframe-text">1 неделя</p>
+        </div>
+        <div className="about__timeframe-block about__timeframe-block_other-weeks">
+          <p className="about__timeframe-text">4 недели</p>
+        </div>
+      </div>
+      <div className="about__timeframe">
+        <div className="about__timeframe-block about__timeframe-block_first-week span">
+          <p className="about__timeframe-text grey-color">Back-end</p>
+        </div>
+        <div className="about__timeframe-block about__timeframe-block_other-weeks span">
+          <p className="about__timeframe-text grey-color">Front-end</p>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import Promo from "./Promo/Promo";
 import NavTab from "./NavTab/NavTab";
+import AboutProject from "./AboutProject/AboutProject";
 
 export default function Main() {
 	return(
 		<>
 			<Promo />
 			<NavTab />
+			<AboutProject />
 		</>
 	)
 }
