b6d86cb8628fdb6e0a4dd8f3b2c6ee9fb61cca86
Konstantin Bartosh <rustigillo@gmail.com>
2023-08-14 17:52:02 +0700

    Feat: add Tech adaptive component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,6 +3,7 @@ import React from "react";
 import Promo from "./Promo/Promo";
 import NavTab from "./NavTab/NavTab";
 import AboutProject from "./AboutProject/AboutProject";
+import Tech from "./Techs/Tech";
 
 export default function Main() {
 	return(
@@ -10,6 +11,7 @@ export default function Main() {
 			<Promo />
 			<NavTab />
 			<AboutProject />
+			<Tech />
 		</>
 	)
 }
