5d6c99fb55cc288d6019e09e224b517d5632e72b
Konstantin Bartosh <rustigillo@gmail.com>
2023-08-19 12:12:18 +0700

    Version 1.0 done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,28 +10,31 @@ import SavedMovies from "../SavedMovies/SavedMovies";
 import Profile from "../Profile/Profile";
 import Login from "../Login/Login";
 import Register from "../Register/Register";
-import Preloader from "../Preloader/Preloader";
+import NotFound from "../NotFound/NotFound";
 
 function App() {
   const url = useLocation();
-  const [loggedIn, setLogin] = useState(true);
+  const [loggedIn, setLogin] = useState(false);
 
   const showFooter =
     url.pathname !== "/profile" &&
     url.pathname !== "/sign-in" &&
     url.pathname !== "/sign-up";
 
-  const showHeader = url.pathname !== "/sign-in" && url.pathname !== "/sign-up";
+  const showHeader =
+    url.pathname !== "/sign-in" &&
+    url.pathname !== "/sign-up";
 
   return (
     <body className="app">
-      {showHeader && <Header loggedIn={loggedIn}/>}
+      {showHeader && <Header loggedIn={loggedIn} />}
       <Routes>
-        <Route path="/sign-up" element={<Register/>} />
-        <Route path="/sign-in" element={<Login/>} />
+        <Route path="/sign-up" element={<Register />} />
+        <Route path="/sign-in" element={<Login />} />
         <Route path="/" element={<Main />} />
         <Route path="/movies" element={loggedIn ? <Movies /> : <Main />} />
-        <Route path="/saved-movies"
+        <Route
+          path="/saved-movies"
           element={loggedIn ? <SavedMovies /> : <Main />}
         />
         <Route path="/profile" element={loggedIn ? <Profile /> : null} />
