c71b1ad7dc647b03ab99b8b0dfc19aa88a173ff3
Konstantin Bartosh <rustigillo@gmail.com>
2023-08-19 18:47:49 +0700

    Fix: change <> for main tag

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,12 +8,12 @@ import Portfolio from "./Portfolio/Portfolio";
 
 export default function Main() {
 	return(
-		<>
+		<main>
 			<Promo />
 			<NavTab />
 			<AboutProject />
 			<Tech />
 			<Portfolio />
-		</>
+		</main>
 	)
 }
