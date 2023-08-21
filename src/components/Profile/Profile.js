65cf37f6ade203cf2bdbc1aa90f60cd11c1c5a6a
Konstantin Bartosh <rustigillo@gmail.com>
2023-08-21 12:50:37 +0700

    Fix: remove fixed bottom

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -7,21 +7,23 @@ export default function Profile() {
 
 	return(
 		<section className="profile">
-			<h2 className="profile__title">Привет, {sampleWord}!</h2>
-			<div className="profile__data-set">
-				<div className="profile__data-string">
-					<span className="profile__text">Имя</span>
-					<span className="profile__text">{sampleWord}</span>
+			<div className="profile__container">
+				<div className="profile__data-set">
+					<h2 className="profile__title">Привет, {sampleWord}!</h2>
+					<div className="profile__data-string">
+						<span className="profile__text">Имя</span>
+						<span className="profile__text">{sampleWord}</span>
+					</div>
+					<span className="underline"/>
+					<div className="profile__data-string">
+						<span className="profile__text">E-mail</span>
+						<span className="profile__text">{sampleWord}</span>
+					</div>
 				</div>
-				<span className="underline"/>
-				<div className="profile__data-string">
-					<span className="profile__text">E-mail</span>
-					<span className="profile__text">{sampleWord}</span>
+				<div className="profile__buttons">
+					<button className="profile__btn">Редактировать</button>
+					<button className="profile__btn profile__btn_red">Выйти из аккаунта</button>
 				</div>
-			</div>
-			<div className="profile__buttons">
-				<button className="profile__btn">Редактировать</button>
-				<button className="profile__btn profile__btn_red">Выйти из аккаунта</button>
 			</div>
 		</section>
 	);
