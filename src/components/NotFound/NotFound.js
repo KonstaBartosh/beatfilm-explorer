448a7f01214160ccabbf3a11e463ebd76c7e50bd
Konstantin Bartosh <rustigillo@gmail.com>
2023-08-21 11:47:50 +0700

    Fix: remove fixed position for naLink & underline

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -7,8 +7,11 @@ export default function NotFound() {
 	return(
 		<div className="notfound">
 			<div className="notfound__container">
-				<h1 className="notfound__title">404</h1>
-				<p className="notfound__subtitle">Страница не найдена</p>
+				<div className="notfound__main">
+					<h1 className="notfound__title">404</h1>
+					<p className="notfound__subtitle">Страница не найдена</p>
+				</div>
+
 				<NavLink to="/" className="notfound__link">Назад</NavLink>
 			</div>
 		</div>
