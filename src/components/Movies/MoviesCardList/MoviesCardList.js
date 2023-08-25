08c13b6f7e8688f03bf117e65d96c522bf74192f
Konstantin Bartosh <rustigillo@gmail.com>
2023-08-25 21:22:56 +0700

    Fix: restore resize listener useEffect

--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -1,54 +1,67 @@
-  import React, { useEffect, useState } from "react";
-  import "./MoviesCardList.css";
-  import MoviesCard from "../MoviesCard/MoviesCard";
-  import Preloader from "../../Preloader/Preloader";
+    import React, { useEffect, useState } from "react";
+    import "./MoviesCardList.css";
+    import MoviesCard from "../MoviesCard/MoviesCard";
+    import Preloader from "../../Preloader/Preloader";
 
-  export default function MoviesCardList({ moviesList, isLoading, errorMessage }) {
-    const [displayCards, setDisplayCards] = useState(16);
-    
-    useEffect(() => {
-      const updateDisplayCards = () => {
-        const screenWidth = window.innerWidth;
-        if (screenWidth >= 1280) {
-          setDisplayCards(16);
-        } else if (screenWidth >= 768) {
-          setDisplayCards(8);
-        } else {
-          setDisplayCards(5);
+    export default function MoviesCardList({ moviesList, isLoading, errorMessage }) {
+      const [displayCards, setDisplayCards] = useState(16);
+      const cardsToShow = moviesList.slice(0, displayCards);
+
+      //** изменение кол-ва карточек в зависимости от ширины экрана */
+      useEffect(() => {
+        const updateDisplayCards = () => {
+          const screenWidth = window.innerWidth;
+          if (screenWidth >= 1280) {
+            setDisplayCards(16);
+          } else if (screenWidth >= 768) {
+            setDisplayCards(8);
+          } else {
+            setDisplayCards(5);
+          }
         }
-      }
+        //** изминение кол-ва при монтировании компонента */
+        updateDisplayCards();
+        
+        //** изменение кол-ва карточек в зав-ти от размера окна */
+        window.addEventListener('resize', () => {
+          console.log(window.innerWidth)
+          updateDisplayCards();
+        });
+
+        return () => {
+          window.removeEventListener("resize", updateDisplayCards);
+        };
+
+      }, []);
+
+      // const cardsToShow = moviesList.slice(0, displayCards);
       
-      updateDisplayCards();
+      //** добавление карточек из списка */
+      const handleAddMoreCards = () => {
+        setDisplayCards(window.innerWidth > 768 ? displayCards + 4 : displayCards + 2);
+      } 
 
-      window.addEventListener('resize', updateDisplayCards);
-
-      return () => {
-        window.removeEventListener("resize", updateDisplayCards);
-      };
-
-    }, []);
-
-    const cardsToShow = moviesList.slice(0, displayCards);
-
-    const handleAddMoreCards = () => {
-      setDisplayCards(displayCards + 4);
-    } 
-
-    if (isLoading) {
-      return (<Preloader />)
+      if (isLoading) {
+        return (<Preloader />)
+      }
+      return (
+        <>
+          <section className="movies-card-list">
+              {cardsToShow.length > 0 ? 
+              (cardsToShow.map((movie) => {
+                return <MoviesCard key={movie.id} movie={movie} />;
+              })) 
+              : (<p className="movies-card-list__message">Ничего не найдено</p>)
+              }
+            {errorMessage}
+          </section>
+          {moviesList.length > cardsToShow.length ?
+          (<button onClick={handleAddMoreCards} className="movies-card-list__button">
+            Еще
+          </button>)
+          :
+          null
+          }
+        </>
+      );
     }
-    return (
-      <>
-        <section className="movies-card-list">
-            {cardsToShow.length > 0 ? 
-            (cardsToShow.map((movie) => {
-              return <MoviesCard key={movie.id} movie={movie} />;
-            })) 
-            : (<p className="movies-card-list__message">Ничего не найдено</p>)
-            }
-          {errorMessage}
-        </section>
-        <button onClick={handleAddMoreCards} className="movies-card-list__button">Еще</button>
-      </>
-    );
-  }
