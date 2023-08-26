6af01ba0ed804da8d87d940ce31a1c9d51fcf1bc
Konstantin Bartosh <rustigillo@gmail.com>
2023-08-26 16:20:06 +0700

    Refactor code style, minor changes

--- src/components/Movies/SearchForm/ToggleSwitch/ToggleSwitch.js
+++ src/components/Movies/SearchForm/ToggleSwitch/ToggleSwitch.js
@@ -3,9 +3,6 @@ import React from "react";
 import "./ToggleSwitch.css";
 
 export default function ToggleSwitch({ onToggle }) {
-	const www = () => console.log('test')
-
-
   return (
 		<label className="toggle">
 			<input type="checkbox" id="toggle"/>
