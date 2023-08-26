aa94448de7c3f1e5966325e2d7f89a3736cb8e1a
Konstantin Bartosh <rustigillo@gmail.com>
2023-08-26 16:55:43 +0700

    Refactor: move mesageError for moviesCardList

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,9 @@
 export const sampleWorkOne = "https://konstabartosh.github.io/how-to-learn/";
 export const sampleWorkTwo = "https://konstabartosh.github.io/russian-travel/";
 export const sampleWorkThree = "https://konstabartosh.github.io/mesto-react-auth/#/sign-in";
+const messageErr = (
+	<p className="movies-card-list__message">
+		Во время запроса произошла ошибка. Возможно, проблема с соединением или
+		сервер недоступен. Подождите немного и попробуйте ещё раз
+	</p>
+);
