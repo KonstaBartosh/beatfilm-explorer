30c400bad44cbf2683bcaf6c87659863dbf497eb
Konstantin Bartosh <rustigillo@gmail.com>
2023-09-08 08:51:17 +0700

    Refactor: add handleToggle for onChange

--- src/components/Movies/SearchForm/ToggleSwitch/ToggleSwitch.js
+++ src/components/Movies/SearchForm/ToggleSwitch/ToggleSwitch.js
@@ -5,8 +5,8 @@ import "./ToggleSwitch.css";
 export default function ToggleSwitch({ onToggle, isToggled }) {
   return (
 		<label className="toggle">
-			<input type="checkbox" id="toggle" checked={isToggled} onChange={() => {}}/>
-			<span onClick={onToggle} className="toggle__slider"></span>
+			<input type="checkbox" id="toggle" checked={isToggled} onChange={onToggle}/>
+			<span className="toggle__slider"></span>
 		</label>
   );
 }
