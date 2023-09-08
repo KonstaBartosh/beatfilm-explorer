698b71938617f1db6648dd75644714134f6d2451
Konstantin Bartosh <rustigillo@gmail.com>
2023-09-08 11:37:42 +0700

    Fix: change path to main page

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 function ProtectedRoute({ element: Component, ...props }) {
-	return props.isLoggedIn ? <Component {...props} /> : <Navigate to="/sign-in" replace />
+	return props.isLoggedIn ? <Component {...props} /> : <Navigate to="/" replace />
 }
 
 export default ProtectedRoute;
