ab937fa8dbb5f471f48c7f65b0b54bdbdf7316ea
Konstantin Bartosh <rustigillo@gmail.com>
2023-09-18 19:24:39 +0700

    Feat: import HashRouter for GH pages deploy

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
-import './index.css';
-import App from './components/App/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, HashRouter } from "react-router-dom";
+import "./index.css";
+import App from "./components/App/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
-      <React.StrictMode>
-      <App/>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
     </React.StrictMode>
-  </HashRouter>
+  </BrowserRouter>
 );
-
